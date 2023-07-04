58852077c6d3d0a9902de43fef4186f765b2e620
Marta Guza <81906950+martunia880@users.noreply.github.com>
2023-07-04 11:17:36 +0200

    half done header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 export default function Header() {
 	return (
-        <h1>Hejka</h1>
-    );
+		<header className='header'> 
+			<div className='header__img'>
+				<div className='header__shadow'>
+                    <h3 className='header__title'>hejka</h3>
+                    <p className='header__description'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima magnam tempore laboriosam, repellendus impedit voluptate voluptatem maxime quisquam sed neque.</p>
+                    <button className='header__btn'>Zobacz ofertę</button>
+                </div>
+            </div>
+		</header>
+	);
 }
